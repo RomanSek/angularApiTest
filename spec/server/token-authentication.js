796329205a6c5b3144182c7835ba5877d73f66d6
Roman Sęk <r.sek@clearcode.cc>
2015-02-23 12:34:50 +0100

    Added Licence to all src files

--- spec/server/token-authentication.js
+++ spec/server/token-authentication.js
@@ -1,3 +1,24 @@
+/**
+ * Copyright (C) 2015 by Clearcode <http://clearcode.cc>
+ * and associates (see AUTHORS).
+ *
+ * This file is part of [angularApiTest].
+ *
+ * [angularApiTest] is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * [angularApiTest] is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with [angularApiTest].  If not, see <http://www.gnu.org/licenses/>.
+ */
+'use strict';
+
 var shmock = require('shmock'),
     handlers = [],
     mock;
